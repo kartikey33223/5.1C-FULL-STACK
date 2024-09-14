31cb126ebd4b7c9808f2971c9a4bba00d7573665
kartikey33223 <Kartikey4786.be23@chitkara.edu.in>
2024-09-15 02:38:56 +0530

    Add files via upload

---
+++ PostButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+
+const PostButton = () => (
+  <Button primary floated="right">
+    Post
+  </Button>
+);
+
+export default PostButton;
